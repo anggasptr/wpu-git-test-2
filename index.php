7e161b9115e5e22bb5f5efcd0233a4ebdc38bd5c
anggasptr <saputraa170503@gmail.com>
2022-01-27 20:08:23 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -13,6 +13,10 @@
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet, veniam culpa! Cum excepturi aut expedita
         incidunt eaque odio, voluptatem maxime animi veritatis quidem laudantium nisi dicta nam saepe necessitatibus
         porro.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
